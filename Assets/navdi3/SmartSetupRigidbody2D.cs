418ebe9526b55e16ea66db33b6061bb86832b3df
droqen <hi@droqen.com>
2019-09-08 09:04:08 -0400

    added navdi3

---
+++ Assets/navdi3/SmartSetupRigidbody2D.cs
@@ -0,0 +1,30 @@
+﻿namespace navdi3
+{
+
+    using UnityEngine;
+    using System.Collections;
+
+    [RequireComponent(typeof(Rigidbody2D))]
+    [ExecuteAlways]
+
+    public class SmartSetupRigidbody2D : MonoBehaviour
+    {
+
+        public Rigidbody2D body { get { return GetComponent<Rigidbody2D>(); } }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (body != null)
+            {
+                body.freezeRotation = true;
+                body.bodyType = RigidbodyType2D.Dynamic;
+                body.gravityScale = 0;
+                body.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+
+                Object.DestroyImmediate(this);
+            }
+        }
+    }
+
+}
---
+++ Assets/navdi3/jump/JumperExamplePlayer.cs
@@ -0,0 +1,23 @@
+﻿namespace navdi3.jump
+{
+
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class JumperExamplePlayer : MonoBehaviour
+    {
+        public Jumper jumper { get { return GetComponent<Jumper>(); } }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space)) jumper.PinJump();
+        }
+        private void FixedUpdate()
+        {
+            jumper.pin = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+            if (!Input.GetKey(KeyCode.Space)) jumper.PinJumpRelease();
+        }
+    }
+
+}
